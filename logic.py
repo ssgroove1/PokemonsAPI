b95b32526faaf9a8d15a3266865c4cf739da7dff
ss_groove <vsevolodvolk2010@gmail.com>
2025-07-19 17:07:47 +0300

    first patch

--- logic.py
+++ logic.py
@@ -11,12 +11,21 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.ability = self.get_ability()
+        self.weight = self.get_weight()
+        self.link = self.get_link()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data["sprites"]['other']['showdown']["front_default"]
+        else:
+            return "No image" # Если будет ошибка 404 или 505 и тд то пишет "no image"
     
     # Метод для получения имени покемона через API
     def get_name(self):
@@ -24,18 +33,54 @@ class Pokemon:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data['forms'][0]['name'])
+            return (data['forms'][0]['name']) # [0] - нулевой список
         else:
-            return "Pikachu"
+            return "Pikachu" # Если будет ошибка 404 или 505 и тд то пишет "Pikachu"
+        
+    def get_ability(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['abilities'][0]['ability']['name']) # [0] - нулевой список
+        else:
+            return "error"
+        
+    def get_weight(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data["weight"])
+        else:
+            return "?"
+        
+    def get_link(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['id'])
+        else:
+            return "bug"
 
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя вашего покеомона: {self.name}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
+    def show_ability(self):
+        return f"Способность вашего покеомона: {self.ability}"
+    
+    def show_weight(self):
+        return f"Вес вашего покеомона: {self.weight} КГ"
+    
+    def show_link(self):
+        return f'Больше информации о вашем покеомоне тут: https://pokeapi.co/api/v2/pokemon/{self.link}'
 
 
 
--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
-        bot.send_photo(message.chat.id, pokemon.show_img())
+        bot.send_video(message.chat.id, pokemon.show_img())
+        bot.send_message(message.chat.id, f'{pokemon.show_ability()}\n{pokemon.show_weight()}')
+        bot.send_message(message.chat.id, pokemon.show_link())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
