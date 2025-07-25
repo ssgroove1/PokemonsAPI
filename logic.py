7d2db38ccde35edd96607845ff6038befee7d3a0
ss_groove <vsevolodvolk2010@gmail.com>
2025-07-25 10:43:58 +0300

    second patch

--- logic.py
+++ logic.py
@@ -4,16 +4,25 @@ import requests
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer):
+    def __init__(self, pokemon_trainer): # pokemon_trainer это и есть Pokemon(message.from_user.username)
 
         self.pokemon_trainer = pokemon_trainer   
 
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
+        self.shinyimg = self.get_shiny_img()
         self.name = self.get_name()
         self.ability = self.get_ability()
         self.weight = self.get_weight()
-        self.link = self.get_link()
+
+        self.poklevels = 1
+
+        self.randomhp = randint(200,400)
+        self.hp = self.randomhp
+        self.reserhp = self.randomhp
+        
+        self.power = randint(30,60)
+
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -26,6 +35,15 @@ class Pokemon:
             return data["sprites"]['other']['showdown']["front_default"]
         else:
             return "No image" # Если будет ошибка 404 или 505 и тд то пишет "no image"
+        
+    def get_shiny_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data["sprites"]['other']['showdown']["front_shiny"]
+        else:
+            return "No shiny image" # Если будет ошибка 404 или 505 и тд то пишет "no image"
     
     # Метод для получения имени покемона через API
     def get_name(self):
@@ -54,33 +72,100 @@ class Pokemon:
             return (data["weight"])
         else:
             return "?"
-        
-    def get_link(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['id'])
-        else:
-            return "bug"
 
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя вашего покеомона: {self.name}"
+        data = (
+            f"Имя вашего покемона: {self.name}\n"
+            f"Сила покемона: {self.power}\n"
+            f"Здоровье покемона: {self.hp}\n"
+            f"Уровень покемона: {self.poklevels}"
+        )
+        return data
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
     
+    def show_shiny_img(self):
+        return self.shinyimg
+    
     def show_ability(self):
-        return f"Способность вашего покеомона: {self.ability}"
+        return f"Способность: {self.ability}"
     
     def show_weight(self):
-        return f"Вес вашего покеомона: {self.weight} КГ"
+        return f"Вес: {self.weight} КГ"
     
-    def show_link(self):
-        return f'Больше информации о вашем покеомоне тут: https://pokeapi.co/api/v2/pokemon/{self.link}'
+
+    # Метод атаки покемона
+    def attack(self, enemy):
+        if isinstance(enemy, Wizzard):
+            chance = randint(1,5)
+            if chance == 3:
+                return "Покемон-волшебник применил щит в сражение"
+
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        
+        else:
+            enemy.hp = 0
+            self.hp = self.reserhp
+            if self.poklevels >= 100:
+                self.poklevels = 100
+                return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}!\nВаш покемон достиг максимального уровня: {self.poklevels} "
+            else:
+                self.poklevels += randint(1,3)
+                return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}!\nНовый уровень вашего покемона: {self.poklevels} "
+        
+
+    # Метод комрления
+    #def tofeed(self):
+    #    if self.poklevels >= 100:
+    #        return f"Ваш покемон достиг максимального уровня!\nУровень вашего покемона: {self.poklevels}"
+    #    else:
+    #        f = self.poklevels + randint(2,7)
+    #        print(f)
+    #        return f"Вы покормили вашего покемона!\nНовый уровень вашего покемона: {self.poklevels}"
+
+
+class Wizzard(Pokemon):
+    def attack(self, enemy):
+        ghh1 = randint(1,2)
+        if ghh1 == 1:
+            magic_power = randint(3,14)
+            self.power += magic_power
+            result = super().attack(enemy)
+            self.power -= magic_power
+            totalpower = magic_power+self.power
+            return result + f"\nМаг применил магический удар силою {totalpower}"
+        else:
+            result = super().attack(enemy)
+            return result
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        ghh = randint(1,3)
+        if ghh == 2:
+            super_power = randint(7,23)
+            self.power += super_power
+            result = super().attack(enemy)
+            self.power -= super_power
+            totalpower = super_power+self.power
+            return result + f"\nБоец применил супер-атаку силою {totalpower}"
+        else:
+            result = super().attack(enemy)
+            return result
 
 
 
+#if __name__ == '__main__':
+    #wizard = Wizzard("username1")
+    #fighter = Fighter("username2")
+
+    #print(wizard.info())
+    #print()
+    #print(fighter.info())
+    #print()
+    #print(fighter.attack(wizard))
--- main.py
+++ main.py
@@ -1,21 +1,109 @@
-import telebot 
+import telebot
+from random import randint
 from config import token
 
 from logic import Pokemon
+from logic import Wizzard
+from logic import Fighter
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
-        bot.send_message(message.chat.id, pokemon.info())
-        bot.send_video(message.chat.id, pokemon.show_img())
+        chance = randint(1,3)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username) # Покемон от юзера
+            bot.send_message(message.chat.id, f'Класс вашего покемона: Стандартный')
+        elif chance == 2:
+            pokemon = Wizzard(message.from_user.username)
+            bot.send_message(message.chat.id, f'Класс вашего покемона: Маг')
+        else:
+            pokemon = Fighter(message.from_user.username)
+            bot.send_message(message.chat.id, f'Класс вашего покемона: Боец')
+        
+        if pokemon.get_weight() >= 1000:
+            pokemon.power += randint(45,95)
+            pokemon.hp += randint(95,225)
+            bot.send_message(message.chat.id, f"{pokemon.info()}\nВам выпал <b>ЛЕГЕНДАРНЫЙ</b> покемон!", parse_mode='HTML')
+            bot.send_video(message.chat.id, pokemon.show_shiny_img())
+
+        elif pokemon.get_weight() >= 500 and pokemon.get_weight() < 1000:
+            pokemon.power += randint(25,45)
+            pokemon.hp += randint(65,85)
+            bot.send_message(message.chat.id, f"{pokemon.info()}\nВам выпал <b>РЕДКИЙ</b> покемон!", parse_mode='HTML')
+            bot.send_video(message.chat.id, pokemon.show_img())
+        else:
+            bot.send_message(message.chat.id, f'{pokemon.info()}\nВам выпал <b>ОБЫЧНЫЙ</b> покемон!', parse_mode='HTML')
+            bot.send_video(message.chat.id, pokemon.show_img())
+
         bot.send_message(message.chat.id, f'{pokemon.show_ability()}\n{pokemon.show_weight()}')
-        bot.send_message(message.chat.id, pokemon.show_link())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
+#@bot.message_handler(commands=['feed'])
+#def feed(message):
+#    pokemon = Pokemon(message.from_user.username)
+#    pokemon = Wizzard(message.from_user.username)
+#    pokemon = Fighter(message.from_user.username)
+#    if message.from_user.username not in Pokemon.pokemons.keys():
+#        bot.send_message(message.chat.id, f"У вас нету покемона!")
+#    else:
+#        bot.send_message(message.chat.id, f"{pokemon.tofeed()}")
+
+
+
+# Показ покеомона
+@bot.message_handler(commands=['info'])
+def info(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username] # Покемон от игрока
+        
+        if isinstance(pok, Pokemon):
+            bot.send_message(message.chat.id, f'Класс вашего покемона: Стандартный')
+        elif isinstance(pok, Wizzard):
+            bot.send_message(message.chat.id, f'Класс вашего покемона: Маг')
+        else:
+            bot.send_message(message.chat.id, f'Класс вашего покемона: Боец')
+
+        if pok.get_weight() >= 1000:
+            bot.send_message(message.chat.id, f"{pok.info()}\nУ вас <b>ЛЕГЕНДАРНЫЙ</b> покемон!", parse_mode='HTML')
+            bot.send_video(message.chat.id, pok.show_shiny_img())
+
+        elif pok.get_weight() >= 500 and pok.get_weight() < 1000:
+            bot.send_message(message.chat.id, f"{pok.info()}\nУ вас <b>РЕДКИЙ</b> покемон!", parse_mode='HTML')
+            bot.send_video(message.chat.id, pok.show_img())
+        else:
+            bot.send_message(message.chat.id, f'{pok.info()}\nУ вас <b>ОБЫЧНЫЙ</b> покемон!', parse_mode='HTML')
+            bot.send_video(message.chat.id, pok.show_img())
+
+        
+        bot.send_message(message.chat.id, f'{pok.show_ability()}\n{pok.show_weight()}')
+        print(message.from_user.username)
+    else:
+        bot.send_message(message.chat.id, "Создайте вашего покемона!")
+
+
+
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            if message.from_user.username:
+                if enemy.hp <= 0:
+                    bot.send_message(message.chat.id, f"{res}\nВраг потерял весь свой уровень, хп!")
+                    enemy.poklevels = 1
+                    enemy.hp = enemy.reserhp
+                else:
+                    bot.send_message(message.chat.id, f"{res}\nУ врага осталось {enemy.hp} единиц здоровья")
+            else:
+                bot.send_message(message.chat.id, f"{res}\nУ врага осталось {enemy.hp} единиц здоровья")
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 
 bot.infinity_polling(none_stop=True)
-
